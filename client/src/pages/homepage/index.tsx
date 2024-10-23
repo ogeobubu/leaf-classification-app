df055b1b70b6a069dbcf2eb16ac9024debe02b2a
ogeobubu <ogeobubu@gmail.com>
2024-10-23 06:36:03 +0100

    recent changes

--- client/src/pages/homepage/index.tsx
+++ client/src/pages/homepage/index.tsx
@@ -14,12 +14,12 @@ const Homepage = () => {
       <div className="overlay absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
       <div className="above-overlay absolute top-0 left-0 w-full h-full z-10 flex flex-col justify-between">
         <div className="md:max-w-4xl p-4 md:p-0 flex flex-col flex-grow mx-auto justify-center items-center">
-        <h1 className="text-4xl text-white my-8">
+        <h1 className="md:text-4xl text-3xl text-white my-8">
           Leaf Identification System
         </h1>
         <div className="flex flex-col justify-center items-center flex-grow">
-          <p className="text-white text-2xl">
-            <strong className="text-4xl">Welcome</strong> to our leaf
+          <p className="text-white md:text-2xl text-xl">
+            <strong className="md:text-4xl text-3xl">Welcome</strong> to our leaf
             identification system. This tool is designed to help you easily
             identify plants based on their leaves. Simply upload an image of a
             leaf, and our advanced algorithms will analyze its unique features
@@ -27,7 +27,7 @@ const Homepage = () => {
           </p>
         </div>
         <div className="flex md:flex-row flex-col gap-3 justify-between items-center w-full mb-8">
-          <Button className="w-[100%]" href="/upload">Identify Leaf</Button>
+          <Button className="w-[300px]" href="/upload">Identify Leaf</Button>
           <Button>Learn More</Button>
           <Button>About Us</Button>
         </div>
